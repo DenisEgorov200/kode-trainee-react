c3df982e66afb23b6b07c3e4d6ea304626c8f8f0
DenisEgorov200 <egorov.denis.mem@gmail.com>
2023-06-29 13:59:46 +0300

    feat(fetching): add loader when fetching

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -11,12 +11,12 @@ import styles from './Users.module.scss';
 export const Users = () => {
   const userType = useSelector((state) => state.category.category.type);
 
-  const { data: users, isLoading, isError } = useGetUsersQuery({ example: userType });
+  const { data: users, isLoading, isFetching, isError } = useGetUsersQuery({ example: userType });
 
   return (
     <div className={styles.users}>
       {isError && <ErrorBoundary />}
-      {isLoading && <UserSkeleton cards={1} />}
+      {(isLoading || isFetching) && <UserSkeleton cards={8} />}
       {users?.items?.map((user) => (
         <User key={user.id} user={user} />
       ))}
